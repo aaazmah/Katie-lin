ffef2284089c3c522c228b18e674531c7b1b847a
Hamza Sheikh <hamza@Hamzas-MacBook-Pro.local>
2025-05-01 16:27:32 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from './components/Header'
+import AnimeRoutes from './components/AnimRoutes'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { motion } from 'framer-motion'
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <AnimeRoutes />
+      </Router>
+    </>
+  )
 };
 
 export default App;
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {
+  ImFacebook,
+  ImPinterest,
+  ImInstagram,
+} from 'react-icons/im'
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+   <ul className='flex gap-x-4'>
+      <li>
+        <a href='https://www.facebook.com' target='_blank'>
+          <ImInstagram/> 
+        </a>
+      </li>
+      <li>
+        <a href='https://www.facebook.com' target='_blank'>
+          <ImPinterest/> 
+        </a>
+      </li>
+      <li>
+        <a href='https://www.facebook.com' target='_blank'>
+          <ImFacebook/> 
+        </a>
+      </li>
+    </ul> 
+  </div>;
 };
 
 export default Socials;
